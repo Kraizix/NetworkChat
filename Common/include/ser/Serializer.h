de80b53ad5edc62dff49d619011fe8eabaeb2417
Dorian Fonseca <71253089+Dorian2002@users.noreply.github.com>
2025-06-29 18:15:43 +0200

    Fix : string serialize/deserialize. Add : string/int vector serialize/deserialize.

--- Common/include/ser/Serializer.h
+++ Common/include/ser/Serializer.h
@@ -6,8 +6,6 @@
 
 namespace ser
 {
-	using SerializableType = uint16_t;
-
 	struct Buffer
 	{
 		/// <summary> Buffer raw data </summary>
@@ -19,9 +17,32 @@ namespace ser
 
 	class Serializer
 	{
-		const SerializableType test = 1;
 
-		std::unordered_map<SerializableType, std::function<bool(Buffer&)>> m_readFunctionMap;
+	public:
+		Serializer() {
+		}
+
+		~Serializer() = default;
+
+		/// <summary>
+		/// Copy the object into the buffer.
+		/// </summary>
+		template<typename T>
+		void MemcpyObj(Buffer& buffer, T& obj, size_t size) {
+			std::memcpy(buffer.m_data, &obj, size);
+			buffer.m_data += size;
+			buffer.m_remaining -= size;
+		}
+
+		/// <summary>
+		/// Copy the buffer into the object.
+		/// </summary>
+		template<typename T>
+		void MemcpyBuffer(T& obj, Buffer& buffer, size_t size) {
+			std::memcpy(&obj, buffer.m_data, size);
+			buffer.m_data += size;
+			buffer.m_remaining -= size;
+		}
 
 		/// <summary>
 		/// Read the passed buffer and write the binary data into the object.
@@ -30,44 +51,72 @@ namespace ser
 		/// <param name="buffer"> : The buffer with serialized data</param>
 		/// <param name="obj"> : The object to write back to</param>
 		template <typename T>
-		T Read(Buffer& buffer) {
+		bool Read(Buffer& buffer, T& obj) {
 			buffer.m_data = 0;
 			return T();
 		}
 
-		#pragma region readSpecialization
+	#pragma region readSpecialization
 		template <>
-		std::string Read<std::string>(Buffer& buffer)
+		bool Read<std::string>(Buffer& buffer, std::string& obj)
 		{
 			size_t objLength;
 			size_t lengthSize = sizeof(size_t);
-			memcpy(&objLength, buffer.m_data, lengthSize);
-			buffer.m_data += lengthSize;
-			buffer.m_remaining -= lengthSize;
+			MemcpyBuffer<size_t>(objLength, buffer, lengthSize);
+			obj.resize(objLength);
 
-			std::string obj(reinterpret_cast<char*>(buffer.m_data), objLength);
-
+			obj.assign(reinterpret_cast<char*>(buffer.m_data), objLength);
 			buffer.m_data += objLength;
 			buffer.m_remaining -= objLength;
 
-			return obj;
+			return true;
 		}
 
 		template <>
-		int Read<int>(Buffer& buffer)
+		bool Read<int>(Buffer& buffer, int& obj)
 		{
-			int obj = 0;
 			size_t size = sizeof(int);
 
-			memcpy(&obj, buffer.m_data, size);
-			buffer.m_data += size;
-			buffer.m_remaining -= size;
+			MemcpyBuffer<int>(obj, buffer, size);
 
-			return obj;
+			return true;
 		}
-		#pragma endregion
 
-	public:
+		template <>
+		bool Read<std::vector<int>>(Buffer& buffer, std::vector<int>& obj)
+		{
+			size_t objLength;
+			size_t lengthSize = sizeof(size_t);
+			MemcpyBuffer<size_t>(objLength, buffer, lengthSize);
+
+			obj.reserve(objLength);
+			for (int i = 0; i < objLength; ++i)
+			{
+				int item;
+				Read<int>(buffer, item);
+				obj.push_back(item);
+			}
+			return true;
+		}
+
+		template <>
+		bool Read<std::vector<std::string>>(Buffer& buffer, std::vector<std::string>& obj) {
+			
+			size_t objLength;
+			size_t lengthSize = sizeof(size_t);
+			MemcpyBuffer<size_t>(objLength, buffer, lengthSize);
+
+			obj.reserve(objLength);
+			for (int i = 0; i < objLength; ++i)
+			{
+				std::string item = "";
+				Read<std::string>(buffer, item);
+				obj.emplace_back(std::move(item));
+			}
+			return true;
+		}
+
+	#pragma endregion
 
 		/// <summary>
 		/// Write the passed object binary data into the buffer.
@@ -76,33 +125,32 @@ namespace ser
 		/// <param name="buffer"> : The buffer to write data into</param>
 		/// <param name="obj"> : The object to get the data from</param>
 		/// <returns>Wether or not the data has been successfuly wroten</returns>
+		
 		template <typename T>
-		bool Write(Buffer& buffer, T* obj) {
+		[[nodiscard]] bool Write(Buffer& buffer, T& obj) {
 			buffer.m_data = 0;
 			obj = nullptr;
 			return false;
 		};
 
-		#pragma region writeSpecialization
+	#pragma region writeSpecialization
 		template <>
-		bool Write<const char*>(Buffer& buffer, const char** obj)
+		bool Write<std::string>(Buffer& buffer, std::string& obj)
 		{
-			size_t objLength = std::strlen(*obj);
+			size_t objLength = obj.length();
 			if (!(objLength > 0))
 				return false;
 
 			size_t lengthSize = sizeof(size_t);
 			//Space for characters + Space for string length
 			size_t totalSize = objLength + lengthSize;
-			
+
 			if (buffer.m_remaining < totalSize)
 				return false;
 
-			memcpy(buffer.m_data, &objLength, lengthSize);
-			buffer.m_data += lengthSize;
-			buffer.m_remaining -= lengthSize;
+			MemcpyObj(buffer, objLength, lengthSize);
 
-			memcpy(buffer.m_data, *obj, objLength);
+			std::memcpy(buffer.m_data, obj.data(), objLength);
 			buffer.m_data += objLength;
 			buffer.m_remaining -= objLength;
 
@@ -110,18 +158,59 @@ namespace ser
 		}
 
 		template <>
-		bool Write<int>(Buffer& buffer, int* obj)
+		bool Write<int>(Buffer& buffer, int& obj)
 		{
 			size_t size = sizeof(int);
-			if (buffer.m_remaining < size) 
+			if (buffer.m_remaining < size)
 				return false;
 
-			memcpy(buffer.m_data, obj, size);
-			buffer.m_data += size;
-			buffer.m_remaining -= size;
+			MemcpyObj<int>(buffer, obj, size);
 
 			return true;
 		}
-		#pragma endregion
+		
+		bool VectorWriteSize(Buffer& buffer, size_t vectorSize, size_t vectorTypeSize) {
+			size_t lengthSize = sizeof(size_t);
+			//Check if buffer has enough space for holding the vector size and the raw data
+			size_t totalSize = vectorSize * vectorTypeSize + lengthSize;
+			if (buffer.m_remaining < totalSize)
+				return false;
+
+			MemcpyObj<size_t>(buffer, vectorSize, lengthSize);
+			return true;
+		}
+
+		template <>
+		bool Write<std::vector<int>>(Buffer& buffer, std::vector<int>& obj)
+		{
+			//Writes the size of the vector in the buffer if able to
+			if (!VectorWriteSize(buffer, obj.size(), sizeof(int)))
+				return false;
+
+			for (auto& item : obj)
+			{
+				if (!Write<int>(buffer, item))
+					return false;
+			}
+			return true;
+		}
+
+		template <>
+		bool Write<std::vector<std::string>>(Buffer& buffer, std::vector<std::string>& obj) {
+			//Writes the size of the vector in the buffer if able to
+			//if (!VectorWriteSize(buffer, obj.size(), sizeof(char)))
+				//return false;
+			size_t lengthSize = sizeof(size_t);
+			size_t vectorSize = obj.size();
+			MemcpyObj<size_t>(buffer, vectorSize, lengthSize);
+
+			for (auto& item : obj)
+			{
+				if (!Write<std::string>(buffer, item))
+					return false;
+			}
+			return true;
+		}
+	#pragma endregion
 	};
 }
